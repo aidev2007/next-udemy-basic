8ba78405cbe2738b5205b55e8073543c78143de8
aidev <aidev@ak2.jp>
2025-07-16 09:18:03 +0900

    学習中

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,15 @@
+export default function AboutLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body>
+        <div className="bg-gray-100 h-screen">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
