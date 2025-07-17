f021d7f49804c2e1636a42780f413accd992d11b
aidev <aidev@ak2.jp>
2025-07-17 15:54:46 +0900

    Update page.tsx

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -3,6 +3,14 @@ type Params =  {
     id: string
   }>
 }
+
+export async function generateMetadata({params}: Params) {
+  const { id } = await params
+  return {
+    title: `ブログ記事ID：${id}です`,
+    description: `ブログ記事${id}の詳細が表示されます`,
+  }
+}
 export default async function page({params} : Params) {
   // console.log(params)
   const { id } = await params
