538ed4461909005f94b760c9d689034fffb1c9fe
aidev <aidev@ak2.jp>
2025-07-19 14:16:16 +0900

    zod型バリデーション

--- src/lib/actions/contact.ts
+++ src/lib/actions/contact.ts
@@ -1,10 +1,21 @@
 'use server' 
 import { redirect } from 'next/navigation' 
+import { ContactSchema } from '@/validations/contact' 
 
 export async function submitContactForm(formData: FormData){ 
     const name = formData.get('name') 
     const email = formData.get('email') 
+
     // バリデーション 
+    const validationResult = ContactSchema.safeParse({ name, email }) 
+    // バリデーションエラーの場合 
+    if (!validationResult.success) { 
+        // エラーメッセージの取得 flattenでエラーを見やすく加工 
+        const errors = validationResult.error.flatten() 
+        console.log('サーバー側でエラー', errors) 
+        return {} 
+    }
+    
     // DB登録 
     console.log('送信されたデータ:', {name, email})     
     redirect('/contacts/complete') 
